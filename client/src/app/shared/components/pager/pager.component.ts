068fd6b901fe94ecde58f67cee7b543f9cfc64d2
Joshua-Gaspar <joshuagaspar03@gmail.com>
2021-08-11 09:47:09 -0400

    Created shared Pager and Paging header components with functionality

---
+++ client/src/app/shared/components/pager/pager.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ShopService } from 'src/app/shop/shop.service';
+import { ShopParams } from '../../models/shopParams';
+
+@Component({
+  selector: 'app-pager',
+  templateUrl: './pager.component.html',
+  styleUrls: ['./pager.component.scss']
+})
+export class PagerComponent implements OnInit {
+
+  @Input() totalCount: number;
+  @Input() pageSize:number;
+  @Output() pageChanged = new EventEmitter<number>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onPagerChange(event: any){
+    this.pageChanged.emit(event.page);
+
+  }
+
+}
