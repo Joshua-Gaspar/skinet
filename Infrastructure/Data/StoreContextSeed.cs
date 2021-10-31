7b4e9e407774dd58f1557932a2c3ecacbcfeba89
Joshua-Gaspar <joshuagaspar03@gmail.com>
2021-10-31 06:57:28 -0400

    Publish

--- Infrastructure/Data/StoreContextSeed.cs
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Core.Entities;
@@ -16,9 +17,11 @@ namespace Infrastructure.Data
         {
             try
             {
+                var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
                 if (!context.ProductBrands.Any())
                 {
-                    var brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+                    var brandsData = File.ReadAllText(path + @"/Data/SeedData/brands.json");
 
                     var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
                     foreach (var item in brands)
@@ -30,7 +33,7 @@ namespace Infrastructure.Data
                 }
                 if (!context.ProductTypes.Any())
                 {
-                    var typeData = File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
+                    var typeData = File.ReadAllText(path + @"/Data/SeedData/types.json");
 
                     var types = JsonSerializer.Deserialize<List<ProductType>>(typeData);
 
@@ -43,7 +46,7 @@ namespace Infrastructure.Data
                 }
                 if (!context.Products.Any())
                 {
-                    var productsData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+                    var productsData = File.ReadAllText(path + @"/Data/SeedData/products.json");
 
                     var products = JsonSerializer.Deserialize<List<Product>>(productsData);
 
@@ -54,9 +57,9 @@ namespace Infrastructure.Data
 
                     await context.SaveChangesAsync();
                 }
-                 if (!context.DeliveryMethods.Any())
+                if (!context.DeliveryMethods.Any())
                 {
-                    var dmData = File.ReadAllText("../Infrastructure/Data/SeedData/delivery.json");
+                    var dmData = File.ReadAllText(path + @"/Data/SeedData/delivery.json");
 
                     var methods = JsonSerializer.Deserialize<List<DeliveryMethod>>(dmData);
 
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -7,13 +7,14 @@ import { map } from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
 import { of, Subject } from 'rxjs';
 import { IProduct } from '../shared/models/product';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
 
-  baseUrl = "https://localhost:5001/api/"
+  baseUrl = environment.apiUrl;
 
   products: IProduct[]=[];
   brands :IBrand[] =[];
